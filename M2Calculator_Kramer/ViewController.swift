62dc40e6b7bf9eaae4c73a3607e40ef90a1879ce
Jenna Kramer <jenna_Kramer11@mymail.eku.edu>
2026-01-27 11:24:41 -0500

    Initial Commit

---
+++ M2Calculator_Kramer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2Calculator_Kramer
+//
+//  Created by Kramer, Jenna L. on 1/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
