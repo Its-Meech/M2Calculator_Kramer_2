487cc26f26a8fa7e4e8c0809373dd0abf4a77215
Jenna Kramer <jenna_Kramer11@mymail.eku.edu>
2026-01-29 11:58:30 -0500

    1/29/26 Commit

--- M2Calculator_Kramer/ViewController.swift
+++ M2Calculator_Kramer/ViewController.swift
@@ -9,6 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var operand1TextField: UITextField!
+    @IBOutlet weak var operand2TextField: UITextField!
+    @IBOutlet weak var selectOperator: UIButton!
+    @IBAction func operatorButton(_sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let plusAction = UIAlertAction(title: "+", style: .default){ _ in
+            self.selectOperator.setTitle("+", for: .normal)
+        }
+        actionSheet.addAction(plusAction)
+        
+        let minusAction = UIAlertAction(title: "-", style: .default){ _ in
+            self.selectOperator.setTitle("-", for: .normal)
+        }
+        actionSheet.addAction(minusAction)
+        
+        let multAction = UIAlertAction(title: "*", style: .default){ _ in
+            self.selectOperator.setTitle("*", for: .normal)
+        }
+        
+        actionSheet.addAction(multAction)
+        
+        let divAction = UIAlertAction(title: "/", style: .default){ _ in
+            self.selectOperator.setTitle("/", for: .normal)
+        }
+        actionSheet.addAction(divAction)
+        
+        
+        present(actionSheet, animated: true)
+    }
+
+    @IBOutlet weak var reultLabel: UILabel!
+    
+    @IBAction func resultButto(_ sender: Any) {
+        let op1 = Int(operand1TextField.text!)!
+        let op2 = Int(operand2TextField.text!)!
+        let op = selectOperator.title(for: .normal)
+        
+        if op == "+" {
+            let result = op1 + op2
+            reultLabel.text = "\(result)"
+        } else if op == "-" {
+            let result = op1 - op2
+            reultLabel.text = "\(result)"
+        } else if op == "*" {
+            let result = op1 * op2
+            reultLabel.text = "\(result)"
+        } else if op == "/" {
+            let result = op1 / op2
+            reultLabel.text = "\(result)"
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
